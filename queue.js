b1a546d3f938ee58f6299eabdab493d14644c56d
John Campbell <campbj49@gmail.com>
2023-08-23 12:39:48 -0400

    queue test runs without errors

--- queue.js
+++ queue.js
@@ -20,27 +20,48 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
-
+    //first make the node to be added
+    let newNode = new Node(val);
+    //handle first value getting added
+    if(this.size ===0) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size++;
+    }
+    else{
+      this.last.next = newNode;
+      this.last = newNode;
+      this.size++;
+    }
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    //first check if it's an empty list
+    if(this.size === 0) throw new Error("ERROR: Cannot dequeue and empty list");
+    //otherwise update the head node and return the old head
+    let oldFirst = this.first;
+    this.first = this.first.next;
+    this.size --;
+    return oldFirst.val;
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    return this.first.val;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
 module.exports = Queue;
+let queue = new Queue();
+queue.enqueue(10);
+queue.enqueue(100);
